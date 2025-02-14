4040d4ef8d3054fbc75ab5bf169fc0d2f6b476e6
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 22:45:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("OOK 2");
+import express, { query } from 'express';
+
+import http from 'http';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const server = http.createServer(app);
+const io = new socketIo(server);
+
+server.listen(8000, () => {
+    console.log("Server is running on http://localhost:8000");
+});
