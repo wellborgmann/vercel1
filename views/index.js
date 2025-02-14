f87103c3b1b797c5f39bac83825357346f33f9c7
wellborgmann <40129426+wellborgmann@users.noreply.github.com>
2025-02-13 22:40:50 -0300

    Create index.js

---
+++ views/index.js
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Code do WhatsApp Web</title>
+ 
+
+</head>
+<body>
+    <h1>EOOI</h1>
+    <div id="qrcode-container">
+        <!-- O QR Code será exibido aqui -->
+        <img id="qrcode" src="" alt="QR Code" />
+    </div>
+
+   
+    <script>
+
+    </script>
+</body>
+</html>
